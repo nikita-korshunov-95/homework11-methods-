a010dc4126b36a96eea076895e776af07de301ca
Nikita <nikita.korshunov.95@mail.ru>
2023-10-29 12:40:34 +0500

    homework 11 (methods)

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,77 @@
 public class Main {
+    public static void printInfoAboutYear(int y) {
+        if (y % 4 == 0 && y % 100 != 0 || y % 400 == 0) {
+            System.out.println("Високосный год");
+        } else {
+            System.out.println("Невисокосный год");
+        }
+    }
+
+    public static void printInfoAboutVersion(int x, int z) {
+        int currentYear = 2023;
+        if (z == 0) {
+            if (x < currentYear) {
+                System.out.println("Установите облегченную версию для Айфона");
+            } else {
+                System.out.println("Установите текущую версию для Айфона");
+            }
+        } else {
+            if (x < currentYear) {
+                System.out.println("Установите облегченную версию для Андроид");
+            } else {
+                System.out.println("Установите текущую версию для Андроид");
+            }
+        }
+    }
+
+    public static int delivery(int distance) {
+        int days = 0;
+        if (distance < 20) {
+            days++;
+            return days;
+        } else if (distance < 60) {
+            days += 2;
+            return days;
+        } else if (distance <= 100) {
+            days += 3;
+            return days;
+        }
+        return days;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+        task2();
+        task3();
+    }
+
+    public static void task1() {
+        System.out.println("Задача №1");
+        int year = 2008;
+        printInfoAboutYear(year);
+    }
+
+    public static void task2() {
+        System.out.println("Задача №2");
+        int yearOfPhone = 2019;
+        int versionOfPhone = 1;
+        printInfoAboutVersion(yearOfPhone, versionOfPhone);
+    }
+
+    public static void task3() {
+        System.out.println("Задача №3");
+        int distance = 35;
+        if (delivery(distance) > 100) {
+            System.out.println("Доставки нет");
+
+        } else {
+            System.out.println("Доставка займет дней: " + delivery(distance));
+
+        }
     }
 }
+
+
+
+
+
